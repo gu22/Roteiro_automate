0cb6e33a89584381b9b2517c830eddc6f756bd94
Gustavo Santos <gu.santos91@gmail.com>
2020-09-18 16:11:22 -0300

    Update roteiro_automate.py

--- roteiro_automate.py
+++ roteiro_automate.py
@@ -20,19 +20,91 @@ elem = driver.find_element_by_name('loginfmt')
 # elem.clear()
 elem.send_keys("gustavo.dossantos@bayer.com")
 elem.send_keys(Keys.RETURN)
+
+
+time.sleep(8)
+
+try:
+    smart = driver.find_element_by_xpath("/html/body/table/tbody/tr/td/div[1]/table/tbody[2]/tr/td/div/table/tbody/tr[1]/td/a[2]")
+    smart.click()
+except NoSuchElementException:
+    pass
+
+
+dt = easygui.enterbox(msg='Enter something.', title=' ', default='', strip=True)
+dt = dt.split(",")
+
+dt13=[]
+for i in range(0,13):
+    dt13.append(dt[i])
+    
+try:    
+    for i in range(0,13):
+        dt.remove(dt[i])
+except IndexError:
+    pass
+
+print(dt)
+print("______________________")
+print(dt13)
+print("")
+dt13 = ",".join(dt13)
+print("______________________")
+print(dt13)
+
+
 button = easygui.msgbox("Aguarde","aguarde")
 
+pesquisa = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[1]/div[7]/div/table/tbody/tr/td/table/tbody/tr/td[1]/img").click()
+try:
+    cxtexto = driver.find_element_by_name("isc_TextAreaItem_0")
+    # cxtexto = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[4]/img[3]")
+except NoSuchElementException:
+    cxtexto = driver.find_element_by_xpath("/html/body/div[8]/div[2]/div/div/div[1]/div[1]/div/form/table/tbody/tr[2]/td[2]/nobr/textarea")
+cxtexto.send_keys(dt13)
+cxtexto.send_keys(Keys.RETURN)
+
+time.sleep(20)
 controle = 1
-contador = 50
+contadori = 1
+contadorf = 39
 if button == "OK":
-    while controle <= contador:
+    while contadori <= contadorf:
         
         if controle == 14:
-            controle = 2
-        if controle == 13:
-            for i in range(1,14):
-                rolagem = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[4]/img[3]")
-                rolagem.click()
+            dt13=[]
+            for i in range(0,13):
+                dt13.append(dt[i])
+    
+            try:    
+                for i in range(0,13):
+                    dt.remove(dt[i])
+            except IndexError:
+                pass
+            
+            dt13 = ",".join(dt13)            
+            print(dt)
+            print("______________________")
+            print(dt13)
+            print("")
+            
+            
+            
+            fechar = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[1]/div[9]/div/table/tbody/tr/td/table/tbody/tr/td[1]/img").click()
+            time.sleep(5)
+            pesquisa = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[1]/div[7]/div/table/tbody/tr/td/table/tbody/tr/td[1]/img").click()
+            # pesquisa = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[4]/img[3]")
+            cxtexto = driver.find_element_by_name("isc_TextAreaItem_0")
+            cxtexto.send_keys(dt13)
+            cxtexto.send_keys(Keys.RETURN)
+            
+            controle = 1
+            time.sleep(20)
+        # if controle == 13:
+        #     for i in range(1,14):
+        #         rolagem = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[4]/img[3]")
+        #         rolagem = driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[4]/img[3]")
+        #         rolagem.click()
                 
         check = driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[3]/div/table/tbody/tr[%d]/td[1]/div/span'%controle)
         arquivo = driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[3]/div/table/tbody/tr[%d]/td[3]/div'%controle).text
@@ -77,7 +149,7 @@ if button == "OK":
         check = driver.find_element_by_xpath('/html/body/div[4]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[3]/div[3]/div/table/tbody/tr[%d]/td[1]/div/span'%controle)
         check.click()
         controle += 1
-        
+        contadori += 1
         time.sleep(1)
 
 driver.close()
