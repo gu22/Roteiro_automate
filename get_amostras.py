72d30e190158d7e5c7b148d4fdb140e1b61f2881
Gustavo Santos <gu.santos91@gmail.com>
2020-09-23 22:50:29 -0300

    update

--- get_amostras.py
+++ get_amostras.py
@@ -39,19 +39,20 @@ mat= ["SECO","LIQUIDO"]
 #escritor = open('cch_dt.txt','a',encoding="utf-8")
 
 # material= "ROUNDUP ORIGINAL DI L"
-escolha= easygui.indexbox(choices = ("Seco","Liquido"))
+#escolha= easygui.indexbox(choices = ("Seco","Liquido"))
      
 # material = mat[escolha]
 # material= re.findall('[A-Z]+',material)
 # material = " ".join(material)
 
-material = mat[escolha]
+#material = mat[escolha]
+material = "DTs_amostras"
 cd = "cch"    
 
 #range é todas as linha da tabela                  
 for i in range(1871):
     try:
-        if material in arq.loc[i][6]:
+        # if material in arq.loc[i][6]:
             if not arq.loc[i][22] in cidade:
                cidade.append( arq.loc[i][22])
                if not  arq.loc[i][9] in dtx:
@@ -70,10 +71,36 @@ for i in range(1871):
        
                    with open((cd+"_"+material)+".txt","a",encoding="utf-8") as escritor:
                         escritor.write("{} : {} \t{} \n".format(i,(arq.loc[i][22]),(arq.loc[ab][9])))
-        else:
-            print("Passou {}".format(arq.loc[i][22]))
+        # else:
+        #     print("Passou {}".format(arq.loc[i][22]))
     except:
         print(arq.loc[i][22])
+# for i in range(1871):
+#     try:
+#         if material in arq.loc[i][6]:
+#             if not arq.loc[i][22] in cidade:
+#                cidade.append( arq.loc[i][22])
+#                if not  arq.loc[i][9] in dtx:
+#                    dtx.append(arq.loc[i][9])
+#                    print("<<< Entrou {} {} >>>>\n".format((arq.loc[i][22]),(arq.loc[i][9])))
+#                    #escritor.write("{} : {} ,{} \n".format(i,(arq.loc[i][22]),(arq.loc[i][9])))
+#                    with open((cd+"_"+material)+".txt", "a",encoding="utf-8") as escritor:
+#                         escritor.write("{} : {} \t{} \n".format(i,(arq.loc[i][22]),(arq.loc[i][9])))
+#                    #     #print("{} : {} ,{} \n".format(i,(arq.loc[i][22]),(arq.loc[i][9])))
+#                    #     escritor.write("Teste")
+#                else:
+#                    ab = i+1
+#                    dtx.append(arq.loc[ab][9])
+#                    print("<<< Entrou {} {} >>>>\n".format((arq.loc[i][22]),(arq.loc[ab][9])))
+#                    #escritor.write("{} : {} ,{} \n".format(i,(arq.loc[i][22]),(arq.loc[ab][9])))
+       
+#                    with open((cd+"_"+material)+".txt","a",encoding="utf-8") as escritor:
+#                         escritor.write("{} : {} \t{} \n".format(i,(arq.loc[i][22]),(arq.loc[ab][9])))
+#         else:
+#             print("Passou {}".format(arq.loc[i][22]))
+#     except:
+#         print(arq.loc[i][22])
+
         
 #escritor.close()
         
