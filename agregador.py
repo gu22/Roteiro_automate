0a6d81946da730b97435d7f5e89ec85efa74c73e
Gustavo Santos <gu.santos91@gmail.com>
2020-09-23 21:20:04 -0300

    Update agregador.py

--- agregador.py
+++ agregador.py
@@ -13,7 +13,7 @@ import os, glob
 import os.path
 import sys
 import glob
-
+import csv
 import re
 from easygui import *
 
@@ -25,7 +25,30 @@ import shutil
 from shutil import copyfile
 
 
-data = easygui.fileopenbox("Escolha o arquivo de orientacao",default="*.xlsx")
+data = easygui.fileopenbox("Escolha o arquivo de orientacao, xmlx",default="*.xlsx")
+data2 = easygui.fileopenbox("Escolha de verificação, txt",default="*.txt")
+
+
 arq = pd.read_excel(data,0)
 
-arq.loc[1][0]
+cat = []
+with open(data2,"r",encoding=("utf-8")) as txt:
+    
+    for i in txt:
+        cat.append(i)
+ 
+    
+verificador = []
+for i in cat:
+    add = (" ").join(re.findall("[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]\w+",i))
+    verificador.append(add)    
+# for i in range(len(arq.index)):
+    # for g in cat:
+    #     if arq.loc[i][0] in 
+
+# arq = open(data,"r",encoding='utf-8')
+# print(arq.read())
+# print("\n")
+
+#arq.loc[1][0]
+# arq.close()
